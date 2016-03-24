b2b6e1f6d545f4f45766b933db5c76e8b3ed699a
tracer-sec <tracer@hush.com>
2016-03-24 07:23:38 +0000

    More tests

--- LegitTests/UtilsTests.cpp
+++ LegitTests/UtilsTests.cpp
@@ -1,9 +1,24 @@
 #include "catch.hpp"
-
 #include "Utils.h"
 
-TEST_CASE("im_in_the_mood_for_testing")
+using namespace std;
+
+TEST_CASE("basic_string_search_and_replace")
 {
     auto result = Utils::FindAndReplace(L"This is a test, mate", L" test,", L" successful test,");
     REQUIRE(result == L"This is a successful test, mate");
 }
+
+TEST_CASE("basic_string_search_and_replace_missing")
+{
+    auto result = Utils::FindAndReplace(L"This is a test, mate", L" successful ", L" failing ");
+    REQUIRE(result == L"This is a test, mate");
+}
+
+TEST_CASE("basic_hex_test")
+{
+    vector<char> data = { 'a', 'b', 'C', 'D' };
+    auto result = Utils::Hex(data);
+    REQUIRE(result == "61624344");
+}
+
