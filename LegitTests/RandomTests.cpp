beb3526a1fe71242664f13241f4ec8a4e9c2d339
tracer-sec <tracer@hush.com>
2016-03-24 19:44:11 +0000

    More unit tests

---
+++ LegitTests/RandomTests.cpp
@@ -0,0 +1,22 @@
+#include "catch.hpp"
+#include "Random.h"
+
+TEST_CASE("random seed test")
+{
+    auto r0 = RandomGenerator(100);
+    auto int0 = r0.GetInt(0, 8192);
+    auto float0 = r0.GetFloat(0, 8192);
+    auto double0 = r0.GetDouble(0, 8192);
+    auto string0 = r0.GetString(ALPHA_NUMERIC_MIXED, 32);
+
+    r0.Reseed(100);
+    auto int1 = r0.GetInt(0, 8192);
+    auto float1 = r0.GetFloat(0, 8192);
+    auto double1 = r0.GetDouble(0, 8192);
+    auto string1 = r0.GetString(ALPHA_NUMERIC_MIXED, 32);
+
+    REQUIRE(int0 == int1);
+    REQUIRE(float0 == float1);
+    REQUIRE(double0 == double1);
+    REQUIRE(string0 == string1);
+}
--- LegitTests/UtilsTests.cpp
+++ LegitTests/UtilsTests.cpp
@@ -3,22 +3,54 @@
 
 using namespace std;
 
-TEST_CASE("basic_string_search_and_replace")
+TEST_CASE("basic string search and replace")
 {
     auto result = Utils::FindAndReplace(L"This is a test, mate", L" test,", L" successful test,");
     REQUIRE(result == L"This is a successful test, mate");
 }
 
-TEST_CASE("basic_string_search_and_replace_missing")
+TEST_CASE("basic string search and replace missing")
 {
     auto result = Utils::FindAndReplace(L"This is a test, mate", L" successful ", L" failing ");
     REQUIRE(result == L"This is a test, mate");
 }
 
-TEST_CASE("basic_hex_test")
+TEST_CASE("multiple matches string replace")
+{
+    auto result = Utils::FindAndReplace(L"111 test 222 test 333 test 444", L"test", L"hurray");
+    REQUIRE(result == L"111 hurray 222 hurray 333 hurray 444");
+}
+
+TEST_CASE("string replace at start")
+{
+    auto result = Utils::FindAndReplace(L"replacing at the end", L"end", L"END");
+    REQUIRE(result == L"replacing at the END");
+}
+
+TEST_CASE("string replace at end")
+{
+    auto result = Utils::FindAndReplace(L"replacing at the start", L"replacing", L"replaced");
+    REQUIRE(result == L"replaced at the start");
+}
+
+TEST_CASE("string trim")
+{
+    auto result = Utils::Trim(" \t    \n\r\n  this is\r \na\t test\t\t\t\n\r\n  \n");
+    REQUIRE(result == "this is\r \na\t test");
+}
+
+TEST_CASE("basic hex test")
 {
     vector<char> data = { 'a', 'b', 'C', 'D' };
     auto result = Utils::Hex(data);
     REQUIRE(result == "61624344");
 }
+
+TEST_CASE("basic hex test 2")
+{
+    char data[] = { 0x12, 0x34, 0x56, 0x78 };
+    auto result = Utils::Hex(data, sizeof(data));
+    REQUIRE(result == "12345678");
+}
+
 
