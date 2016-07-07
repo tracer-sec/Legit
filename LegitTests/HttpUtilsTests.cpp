dce681dc1038a0c867bceb70c91b81ffe82fc30a
tracer-sec <tracer@hush.com>
2016-07-07 21:58:49 +0100

    Fix test for multipart encoding Dependent on the order of an unordered_map ... whoops

--- LegitTests/HttpUtilsTests.cpp
+++ LegitTests/HttpUtilsTests.cpp
@@ -36,5 +36,26 @@ TEST_CASE("HttpUtils multipart encoding")
     auto body = HttpUtils::CreateBody(fields, files, boundary);
     string result(body.begin(), body.end());
 
-    REQUIRE(result == "");
+    string compare = 
+        "--" + boundary + "\r\n" +
+        "Content-Disposition: form-data; name=\"foo\"\r\n" +
+        "\r\n" +
+        "bar\r\n" +
+        "--" + boundary + "\r\n" +
+        "Content-Disposition: form-data; name=\"baz\"\r\n" +
+        "\r\n" +
+        "123\r\n" +
+        "--" + boundary + "\r\n" +
+        "Content-Disposition: form-data; name=\"f0\"; filename=\"original_f0.txt\"\r\n" +
+        "Content-Type: application/octet-stream\r\n" +
+        "\r\n" +
+        "AAAAAAAAAAAAAAAA\r\n" +
+        "--" + boundary + "\r\n" +
+        "Content-Disposition: form-data; name=\"f1\"; filename=\"original_f1.txt\"\r\n" +
+        "Content-Type: application/octet-stream\r\n" +
+        "\r\n" +
+        "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB\r\n" +
+        "--" + boundary + "--\r\n";
+
+    REQUIRE(result == compare);
 }
